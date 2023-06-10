8359e310b372219efcbe86596c956485ac86dade
nkatshiba <nkatshiba@proton.me>
2023-06-10 14:55:19 +0200

    working now

--- dot_config/dunst/scripts/executable_show_mute.sh
+++ dot_config/dunst/scripts/executable_show_mute.sh
@@ -2,10 +2,10 @@
 
 mute=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print tolower($0)}')
 
-icon="󰕾"
+icon="111111111111111111111111111111111"
 
 if [[ $mute == "mute: yes" ]]; then
-    icon="󰖁"
+	icon="000000000000000000000000000000000"
 fi
 
-notify-send -t 700 "${icon} Volume ${mute}"
+notify-send -t 700 -u normal "${icon}${icon}${icon}"
