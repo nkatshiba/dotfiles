d934fc16ebd15eacb4388fce3ec319c444bacdde
nkatshiba <nkatshiba@proton.me>
2023-06-10 09:24:22 +0200

    nvim surround plugin init

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -1,9 +1,14 @@
 return {
-  { "folke/which-key.nvim", enabled = false },
+  -- { "folke/which-key.nvim", enabled = false },
   {
     "echasnovski/mini.comment",
     enabled = false,
   },
+  {
+    "echasnovski/mini.pairs",
+    enabled = false,
+  },
+
   {
     "ggandor/flit.nvim",
     enabled = false,
---
+++ dot_config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "öa", -- Add surrounding in Normal and Visual modes
+        delete = "öd", -- Delete surrounding
+        find = "ök", -- Find surrounding (to the right)
+        find_left = "öj", -- Find surrounding (to the left)
+        highlight = "öh", -- Highlight surrounding
+        replace = "ör", -- Replace surrounding
+        update_n_lines = "ön", -- Update `n_lines`
+      },
+    },
+  },
+}
