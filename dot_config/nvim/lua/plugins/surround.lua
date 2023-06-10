88660016f642ec26433977b680e3dcec98c5bec1
nkatshiba <nkatshiba@proton.me>
2023-06-10 14:56:05 +0200

    surround binds done

--- dot_config/nvim/lua/plugins/surround.lua
+++ dot_config/nvim/lua/plugins/surround.lua
@@ -5,12 +5,13 @@ return {
       mappings = {
         add = "öa", -- Add surrounding in Normal and Visual modes
         delete = "öd", -- Delete surrounding
-        find = "ök", -- Find surrounding (to the right)
-        find_left = "öj", -- Find surrounding (to the left)
+        find = "öf", -- Find surrounding (to the right)
+        find_left = "öF", -- Find surrounding (to the left)
         highlight = "öh", -- Highlight surrounding
         replace = "ör", -- Replace surrounding
         update_n_lines = "ön", -- Update `n_lines`
       },
+      search_method = "cover_or_next",
     },
   },
 }
